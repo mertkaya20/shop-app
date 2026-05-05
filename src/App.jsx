c957d7696c1f7fc8f29349688c1aa1dc5ab99ffe
mertkaya20 <mertkaya.codes@gmail.com>
2026-05-05 13:39:52 +0300

    feat: router setup and not-found page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import ProductDetail from "./pages/ProductDetail";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/detail/:id" element={<ProductDetail />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default App;
