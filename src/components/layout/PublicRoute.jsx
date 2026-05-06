06ce810309ea56cd9919cd0332418fc83d88dca4
mertkaya20 <mertkaya.codes@gmail.com>
2026-05-06 17:26:59 +0300

    feat: create public route for login and register pages

---
+++ src/components/layout/PublicRoute.jsx
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+const PublicRoute = ({ children }) => {
+  const { token } = useSelector((state) => state.auth);
+  if (token) return <Navigate to="/" replace />;
+  return children;
+};
+
+export default PublicRoute;
